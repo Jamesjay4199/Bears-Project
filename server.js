a3a7aa4a48d9ba52ef6af10a3c79347568f47a22
James John <jamesjay4199@gmail.com>
2017-12-17 22:35:14 +0100

    can now create bears

--- server.js
+++ server.js
@@ -38,7 +38,26 @@ router.get('/', function(req, res){
 
 // more routes for our api will happen here
 
-//register our routes ----------------------------------------------------------------
+//on routes that end in /bears
+//---------------------------------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err){
+                res.send(err);
+            }
+            res.json({ message: 'Bear created!' });
+        }());
+        
+
+    });//register our routes ----------------------------------------------------------------
 //all of our routes will be prefixed with /api
 app.use('/api', router);
 
