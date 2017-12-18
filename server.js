bcf8779f655cc045e38982b0dcff499030c7a650
James John <jamesjay4199@gmail.com>
2017-12-18 22:49:40 +0100

    reworking

--- server.js
+++ server.js
@@ -48,32 +48,21 @@ router.get('/', function(req, res){
 router.route('/bears')
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
+    .post(function (req, res) {
 
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
 
         // save the bear and check for errors
-        bear.save(function(err) {
+        bear.save(function (err) {
             if (err)
                 res.send(err);
-            
+
             res.json({ message: 'Bear created!' });
         });
-        
 
-    })
-
-    //get all the bears
-    .get(function(req, res){
-    	Bear.find(function(err, bears){
-    		if (err) 
-    			res.send(err);
-    	
-    		res.json(bears);
-    	});
     });
-
+    
     //register our routes ----------------------------------------------------------------
 //all of our routes will be prefixed with /api
 app.use('/api', router);
